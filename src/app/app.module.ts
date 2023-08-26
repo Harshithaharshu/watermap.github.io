e56a8be11a5303c5e7a86225008c1fdc78815365
harshitha <arharshitha403@gmail.com>
2023-08-26 12:17:30 +0530

    final push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
+import { HelloComponent } from './hello.component';
+import { CategoryPipe } from './category.pipe'
+import { FormsModule } from '@angular/forms';
+import { TimeSeriesChartComponent } from './time-series-chart/time-series-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    HelloComponent, CategoryPipe, TimeSeriesChartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
